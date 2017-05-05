5ae6ea5f6d5e8a73243da749c416073648826d27
Michael Ly <Michael Ly@128.4.64.161>
2017-05-04 23:59:10 -0400

    Final Commit

--- PS6_DAL/src/test/java/base/Person_Test.java
+++ PS6_DAL/src/test/java/base/Person_Test.java
@@ -2,7 +2,9 @@ package base;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.UUID;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -40,7 +42,46 @@ public class Person_Test {
 		per1.setPostalCode(19734);
 		per1.setStreet("214 Labrador Lane");
 		
+		//AddPerson and GetPerson Tests
 		PersonDAL.addPerson(per1);
+		UUID per1ID = per1.getPersonID();
+		
+		PersonDomainModel per2 = PersonDAL.getPerson(per1ID);
+		assertTrue(per2.getCity().equals(per1.getCity()));
+		assertTrue(per2.getFirstName().equals(per1.getFirstName()));
+		assertTrue(per2.getLastName().equals(per1.getLastName()));
+		assertTrue(per2.getPostalCode().equals(per1.getPostalCode()));
+		assertTrue(per2.getStreet().equals(per1.getStreet()));
+		
+		//GetPersons Test
+		ArrayList<PersonDomainModel> people = PersonDAL.getPersons();
+		
+		PersonDomainModel per3 = new PersonDomainModel();
+		per3.setBirthday(new Date(0));
+		per3.setCity("Hockessin");
+		per3.setFirstName("Michael");
+		per3.setLastName("Ly");
+		per3.setPostalCode(19707);
+		per3.setStreet("28 Clark Ridge");
+		
+		UUID per3ID = per3.getPersonID();
+		PersonDAL.addPerson(per3);
+		people = PersonDAL.getPersons();
+		
+		//UpdatePerson Test
+		per1.setCity("New York");
+		per1.setStreet("221 Wolf Street");
+		PersonDAL.updatePerson(per1);
+		
+		PersonDomainModel per4 = PersonDAL.getPerson(per1ID);
+		assertTrue(per4.getCity().equals("New York"));
+		assertTrue(per4.getStreet().equals("221 Wolf Street"));
+		
+		//DeletePerson Test
+		PersonDAL.deletePerson(per1ID);
+		PersonDAL.deletePerson(per3ID);
+		assertNull(PersonDAL.getPerson(per1ID));
+		assertNull(PersonDAL.getPerson(per3ID));
 		
 		
 	}
